91308aa28f0e4250258f5b1743016687e445035a
Tiago-Fogolin <ticofogolin@gmail.com>
2023-08-27 19:00:57 -0300

    Adicionando navegação entrae

--- app/src/main/java/com/example/drawerproject/MainActivity.kt
+++ app/src/main/java/com/example/drawerproject/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.drawerproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timersBtn = findViewById<Button>(R.id.timers_btn)
+        val openCloseBtn = findViewById<Button>(R.id.open_close_btn)
+
+        timersBtn.setOnClickListener(){
+            var intent = Intent(this@MainActivity,Timers::class.java)
+            startActivity(intent)
+        }
+
+        openCloseBtn.setOnClickListener(){
+            var intent = Intent(this@MainActivity,OpenClose::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/example/drawerproject/OpenClose.kt
@@ -0,0 +1,11 @@
+package com.example.drawerproject
+
+import android.app.Activity
+import android.os.Bundle
+
+class OpenClose: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.open_close)
+    }
+}
