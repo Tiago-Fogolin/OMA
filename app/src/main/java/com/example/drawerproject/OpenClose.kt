e00d03f59a2ce27d9edcda0c9b4cea49230b5311
Tiago-Fogolin <ticofogolin@gmail.com>
2023-08-27 19:19:40 -0300

    Finalizando a navegação

--- app/src/main/java/com/example/drawerproject/OpenClose.kt
+++ app/src/main/java/com/example/drawerproject/OpenClose.kt
@@ -1,11 +1,22 @@
 package com.example.drawerproject
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class OpenClose: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.open_close)
+
+
+        val timersBtn = findViewById<Button>(R.id.timers_btn)
+
+        timersBtn.setOnClickListener(){
+            var intent = Intent(this@OpenClose,Timers::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/drawerproject/Timers.kt
+++ app/src/main/java/com/example/drawerproject/Timers.kt
@@ -1,11 +1,22 @@
 package com.example.drawerproject
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class Timers: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.timers)
+
+        val openCloseBtn = findViewById<Button>(R.id.open_close_btn)
+
+        openCloseBtn.setOnClickListener(){
+            var intent = Intent(this@Timers,OpenClose::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
