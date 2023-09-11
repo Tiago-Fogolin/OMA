aee6ca776f81533eaa31f2030b265e1bb9c7b0c0
LuanM23 <luanmfernandes95@gmail.com>
2023-09-11 15:52:43 -0300

    commit timer

--- app/src/main/java/com/example/drawerproject/OpenCloseButton.kt
+++ app/src/main/java/com/example/drawerproject/OpenCloseButton.kt
@@ -1,4 +1,26 @@
-package com.example.drawerproject
+import android.content.Context
+import android.view.View
+import android.widget.Button
 
-class OpenCloseButton {
+class OpenCloseButton(private val btnId: Int, private val currentView: View) {
+    // Objeto companion para manter um contador estático
+    companion object {
+        private var contador = 1
+    }
+
+    // Inicialização da classe
+    init {
+        val btn = currentView.findViewById<Button>(btnId)
+        val contint = contador
+
+        // Configurar um ouvinte de clique para o botão
+        btn.setOnClickListener {
+            // Quando o botão é clicado, cria uma instância de Connection e envia a mensagem com o contador atual
+            val conn = Connection()
+            conn.sendMessage("${contint}")
+
+            // Incrementa o contador estático
+            contador++
+        }
+    }
 }
--- app/src/main/java/com/example/drawerproject/Timer.kt
+++ app/src/main/java/com/example/drawerproject/Timer.kt
@@ -1,4 +1,24 @@
 package com.example.drawerproject
 
-class Timer {
+
+import android.view.View
+import android.app.TimePickerDialog
+import android.os.Build
+import android.os.Bundle
+import android.widget.TimePicker
+import androidx.annotation.RequiresApi
+
+class Timer (private val timerId: Int, private val currentView: View)  {
+    var timer = this.currentView.findViewById<TimePicker>(this.timerId)
+    init {
+
+        timer.setIs24HourView(true)
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun getTime(): String {
+        val hour = timer.hour.toString()
+        val minute = timer.minute.toString()
+        return ("$hour : $minute")
+    }
 }
+
