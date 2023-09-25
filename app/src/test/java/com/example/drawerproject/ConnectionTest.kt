e32a5876f0f03f65b67ffe78bd89161e02b9c951
Tiago-Fogolin <ticofogolin@gmail.com>
2023-09-25 15:10:43 -0300

    Subindo o teste unitário

---
+++ app/src/test/java/com/example/drawerproject/ConnectionTest.kt
@@ -0,0 +1,43 @@
+import com.example.drawerproject.Connection
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.net.HttpURLConnection
+
+class ConnectionTest {
+
+    private lateinit var server: MockWebServer
+    private lateinit var connection: Connection
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        server.start()
+        val baseUrl = server.url("/")
+        connection = Connection(baseUrl.host, baseUrl.port.toString())
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
+
+    @Test
+    fun testSendMessage() {
+        val message = "Test Message"
+
+        server.enqueue(MockResponse().setResponseCode(HttpURLConnection.HTTP_OK))
+
+        runBlocking {
+            connection.sendMessage(message)
+        }
+
+
+        val request = server.takeRequest()
+        assert(request.method == "POST")
+        assert(request.body.readUtf8() == message)
+    }
+}
